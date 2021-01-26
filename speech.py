a6b358ce627233266296ff3e1233d3b3a10dcf5b
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-01-26 15:40:33 -0800

    Add files via upload

--- speech.py
+++ speech.py
@@ -1,35 +1,10 @@
 import speech_recognition as sr
-from google.cloud import speech
-import io
 
-# obtain audio from the microphone
 r = sr.Recognizer()
+
 with sr.Microphone() as source:
-    print("Say something!")
+    print("say something")
     audio = r.listen(source)
+    voice_data = r.recognize_google(audio)
+    print(voice_data)
 
-# write audio to a FLAC file
-with open("microphone-results.flac", "wb") as f:
-    f.write(audio.get_flac_data())
-
-f.close()
-
-client = speech.SpeechClient()
-
-with io.open("microphone-results.flac", "rb") as audio_file:
-        content = audio_file.read()
-
-    audio = speech.RecognitionAudio(content=content)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-    )
-
-    response = client.recognize(config=config, audio=audio)
-
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        print(u"Transcript: {}".format(result.alternatives[0].transcript))
