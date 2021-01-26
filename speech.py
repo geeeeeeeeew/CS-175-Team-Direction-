052dc8d43943f96a2536a7ee7408149d259bd659
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-01-26 15:55:40 -0800

    Add files via upload

--- speech.py
+++ speech.py
@@ -2,9 +2,26 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("say something")
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+def record_audio():
+    try:
+        with sr.Microphone() as source:
+            print("Say something")
+            audio = r.listen(source)
+            voice_data = r.recognize_google(audio)
+            return voice_data
+    except sr.UnknownValueError:
+        print("Cannot recognize audio")
+    except sr.RequestError:
+        print("Speech service down")
 
+#sample main loop
+if __name__ == "__main__":
+    while True:
+        command = record_audio()
+        print(command)
+
+        #process command
+
+        if "exit" in command:
+            print("Exiting")
+            break
