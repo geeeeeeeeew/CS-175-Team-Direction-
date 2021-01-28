35e524738bbff1af9fb9cd1c73655e279c34701f
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-01-27 22:53:46 -0800

    Update Command.py

--- Command.py
+++ Command.py
@@ -4,20 +4,22 @@ import neuralcoref
 #installing neuralcoref:
 #pip install neuralcoref
 #pip install spacy==2.1.0
-#python -m spacy download en_core_web_md
+#python -m spacy download en_core_web_sm
 
 #check for similiaty with list of supported actions and nouns
 #possible optimization: remove ununsed pipelines in spacy
 #suppot numerical commands "jump 6 times"
 #Processing POS TAGGING, rule based matching, entity recognition (Proper Nouns), lemmatization (reduce words to their base playing -> play)
 
-nlp = spacy.load('en_core_web_md') #use medium sized english model
+nlp = spacy.load('en_core_web_sm') #use small sized english model
 neuralcoref.add_to_pipe(nlp)
 
 class Command:
     def __init__(self, raw_text):
         self.raw_text = raw_text
         self.doc = nlp(raw_text)
+        if self.doc._.has_coref:
+            self.doc = nlp(self.doc._.coref_resolved)
 
     def process(self):
         pass
