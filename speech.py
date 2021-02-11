6734d24f24f29dd8bd4f5d1afffbe08837efbbda
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-02-10 18:11:27 -0800

    Add files via upload

--- speech.py
+++ speech.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-'''
+
 def record_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -7,20 +7,3 @@ def record_audio():
         audio = r.listen(source)
         voice_data = r.recognize_google(audio)
         return voice_data
-
-'''
-import speech_recognition as sr
-
-def record_audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Say something")
-        r.adjust_for_ambient_noise(source, duration = 1)
-        audio = r.listen(source)
-        try:
-            voice_data = r.recognize_google(audio)
-            print('You said:{}'.format(voice_data))
-            return voice_data
-        except:
-            print('sorry, could not recognize your voice')
-            return None
