85e4e8c0b6327f8dd062e5f82b62165036b1301a
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-02-11 00:06:34 -0800

    Add files via upload

--- Process.py
+++ Process.py
@@ -5,10 +5,55 @@ import time
 class Process:
     def __init__(self, malmo):
         self.malmo = malmo
+
+    def process_walk(self, objList, command):
+        dir = 0 #flag to indicate if a direction was specified
+        distance = max([command.parse_numerical(obj) for obj in objList]) #search for the largest numerical value in objList
+        if "left" in objList:
+            self.malmo.walk_left(distance)
+            dir = 1
+        if "right" in objList:
+            self.malmo.walk_right(distance)
+            dir = 1
+        if "backward" in objList or "backwards" in objList:
+            self.malmo.walk_backward(distance)
+            dir = 1
+        if "forward" in objList or dir == 0: # if dir == 0 then no direction was specified so default to forward
+            self.malmo.walk_forward(distance)
+    
+    def process_run(self, objList, command):
+        dir = 0
+        distance = max([command.parse_numerical(obj) for obj in objList])
+        if "left" in objList:
+            self.malmo.run_left(distance)
+            dir = 1
+        if "right" in objList:
+            self.malmo.run_right(distance)
+            dir = 1
+        if "backward" in objList or "backwards" in objList:
+            self.malmo.run_backward(distance)
+            dir = 1
+        if "forward" or dir == 0:
+            self.malmo.run_forward(distance)
+    
+    def process_turn(self, objList, command):
+        if "left" in objList:
+            pass
+        if "right" in objList:
+            pass
+    
+    def process_jump(self, objList, command):
+        num_jumps = max([command.parse_numerical(obj) for obj in objList])
+        self.malmo.jump(num_jumps = num_jumps)
+    
+    def process_crouch(self, objList, command):
+        length = max([command.parse_numerical(obj) for obj in objList])
+        self.malmo.crouch(length = length)
+        
     #the basic flow is to iterate through all the dicts parseList contains
     #then process the objList ties to the verb
     #depending on the verb process the objlist is processed differently
-    #for example input is "walk left 5 blocks" -> [{walk:['left', '5 blocks']}] -> process the objlist to dir = 'left' , dis = 5
+    #for example input is "walk left 5 blocks" -> [{walk:['left', '5 blocks']}] -> process_walk(objList, command) -> processes the objlist to dir = 'left' , dis = 5 -> sends to malmo
     def process_command(self, command):
         parseList = command.parse()
         for c in parseList:
@@ -19,43 +64,14 @@ class Process:
                     objList = [""]
                     #planned supported commands crouch, turn, walk, run, jump
                 if verb == "walk":
-                    dir = 0 #flag to indicate if a direction was specified
-                    distance = max([command.parse_numerical(obj) for obj in objList]) #search for the largest numerical value in objList
-                    if "left" in objList:
-                        self.malmo.walk_left(distance)
-                        dir = 1
-                    if "right" in objList:
-                        self.malmo.walk_right(distance)
-                        dir = 1
-                    if "backward" in objList or "backwards" in objList:
-                        self.malmo.walk_backward(distance)
-                        dir = 1
-                    if "forward" in objList or dir == 0: # if dir == 0 then no direction was specified so default to forward
-                        self.malmo.walk_forward(distance)
+                    self.process_walk(objList, command)
                 elif verb == "run":
-                    dir = 0
-                    distance = max([command.parse_numerical(obj) for obj in objList])
-                    if "left" in objList:
-                        self.malmo.run_left(distance)
-                        dir = 1
-                    if "right" in objList:
-                        self.malmo.run_right(distance)
-                        dir = 1
-                    if "backward" in objList or "backwards" in objList:
-                        self.malmo.run_backward(distance)
-                        dir = 1
-                    if "forward" or dir == 0:
-                        self.malmo.run_forward(distance)
+                    self.process_run(objList, command)
                 elif verb == "turn":
-                    if "left" in objList:
-                        pass
-                    if "right" in objList:
-                        pass
+                    self.process_turn(objList, command)
                 elif verb == "jump":
-                    num_jumps = max([command.parse_numerical(obj) for obj in objList])
-                    self.malmo.jump(num_jumps = num_jumps)
+                    self.process_jump(objList, command)
                 elif verb == "crouch":
-                    length = max([command.parse_numerical(obj) for obj in objList])
-                    self.malmo.crouch(length = length)
+                    self.process_crouch(objList, command)
 
                 time.sleep(0.2)
