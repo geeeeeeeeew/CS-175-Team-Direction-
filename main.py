4df9e3a554ba5049d946ca753084106ad476b7c8
kevinwang0 <kevinw91007@gmail.com>
2021-02-11 02:03:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     move = BasicMovement({})
     while True:
         try:
-            #command =  record_audio() 
-            command = str(input("Type your command: ")) #for keyboard input, uncomment this line and comment out line 10
+            command =  record_audio()
+            #command = str(input("Type your command: ")) #for keyboard input, uncomment this line and comment out line 10
             command = Command("Steve has to " + command)
             #add "steve has to" to the string so spacy understand the command better
             #ie user says "find 2 sheep" spacy parses "Steve has to find 2 sheep"
@@ -34,16 +34,37 @@ if __name__ == "__main__":
         parseList = command.parse()
         print(parseList)
         for c in parseList:
-            print(c)
             for verb in c.keys():
                 objList = c[verb]
                 if not objList: #no dobj specified case
                     objList = [""]
                 for obj in objList:
-                    #if verb == 'walk' and "left" in obj:
-                    if verb == "walk":
+                    if verb == 'walk left' in obj:
+                        print("WALK left", command.parse_numerical(obj))
+                        move.walk_left(distance = command.parse_numerical(obj))
+                    elif verb == 'walk' or verb == 'walk forward':
                         print("WALK", command.parse_numerical(obj))
-                        move.move_forward(distance = command.parse_numerical(obj))
+                        move.walk_forward(distance = command.parse_numerical(obj))
+                    elif verb == 'walk right':
+                        print("WALK right", command.parse_numerical(obj))
+                        move.walk_right(distance = command.parse_numerical(obj))
+                    elif verb == 'walk backward':
+                        print("WALK back", command.parse_numerical(obj))
+                        move.walk_backward(distance = command.parse_numerical(obj))
+
+                    elif verb == 'run left':
+                        print("RUN left", command.parse_numerical(obj))
+                        move.run_left(distance = command.parse_numerical(obj))
+                    elif verb == 'run right':
+                        print("RUN right", command.parse_numerical(obj))
+                        move.run_right(distance = command.parse_numerical(obj))
+                    elif verb == 'run' or verb == 'run forward':
+                        print("RUN", command.parse_numerical(obj))
+                        move.run_forward(distance = command.parse_numerical(obj))
+                    elif verb == 'run backward':
+                        print("RUN backward", command.parse_numerical(obj))
+                        move.run_backward(distance = command.parse_numerical(obj))
+
                     elif verb == "jump":
                         print("JUMP")
                         print(command.parse_numerical(obj))
