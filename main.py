f648132539024ddbf8a46523356e14083b5f8c10
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-02-10 16:33:10 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from speech import record_audio
 from Command import Command
 from speech_recognition import UnknownValueError
 from speech_recognition import RequestError
+from basic_movement import BasicMovement
+import time
 
 #sample main loop
 if __name__ == "__main__":
+    move = BasicMovement({})
     while True:
         try:
-            command =  record_audio() 
-            #command = str(input("Type your command: ")) #for keyboard input, uncomment this line and comment out line 10
+            #command =  record_audio() 
+            command = str(input("Type your command: ")) #for keyboard input, uncomment this line and comment out line 10
             command = Command("Steve has to " + command)
             #add "steve has to" to the string so spacy understand the command better
             #ie user says "find 2 sheep" spacy parses "Steve has to find 2 sheep"
-
             print("raw_text", command.rawText)
             for token in command.doc:
                 print(token.text, end=' ')
-                print(token.dep_, token.pos_)
-            
+                print(token.dep_, token.pos_)    
         except UnknownValueError:
             print("Cannot recognize audio")
             continue
@@ -32,5 +33,21 @@ if __name__ == "__main__":
         
         parseList = command.parse()
         print(parseList)
+        for c in parseList:
+            print(c)
+            for verb in c.keys():
+                objList = c[verb]
+                if not objList: #no dobj specified case
+                    objList = [""]
+                for obj in objList:
+                    #if verb == 'walk' and "left" in obj:
+                    if verb == "walk":
+                        print("WALK", command.parse_numerical(obj))
+                        move.move_forward(distance = command.parse_numerical(obj))
+                    elif verb == "jump":
+                        print("JUMP")
+                        print(command.parse_numerical(obj))
+                        move.jump(num_jumps=command.parse_numerical(obj))
+        time.sleep(.2)
 
         #execute list of commands in Malmo after processing
