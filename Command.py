f458be7b69e6468063dfdf1522ed02d6a097731a
jethaiUCI <65882284+jethaiUCI@users.noreply.github.com>
2021-02-24 19:55:55 -0800

    Add files via upload

--- Command.py
+++ Command.py
@@ -6,11 +6,6 @@ import neuralcoref
 #pip install spacy==2.1.0
 #python -m spacy download en_core_web_md
 
-#check for similiaty with list of supported actions and nouns
-#possible optimization: remove ununsed pipelines in spacy
-#suppot numerical commands "jump 6 times"
-#Processing POS TAGGING, rule based matching, entity recognition (Proper Nouns), lemmatization (reduce words to their base playing -> play)
-
 nlp = spacy.load('en_core_web_md') #use medium sized english model
 #nlp.Defaults.stop_words |= {'a','an','the', 'to'} 
 #nlp.add_pipe(nlp.create_pipe('merge_noun_chunks'))
@@ -19,7 +14,7 @@ neuralcoref.add_to_pipe(nlp)
 
 class Command:
     filterWords = {'a','an','the', 'to', 'then', 'for', 'in', 'on', 'at', 'by'} #static class atribute
-    actions = {'move': ['jump', 'walk', 'crouch', 'run']} #planned supported actions for status report
+    actions = {'move': ['jump', 'walk', 'crouch', 'run', 'find']}
     #used a dict so similarity checks for a category of actions (keys) and then searches for specfic supported actions(values)
     #reduces search to a category of actions instead the entire range of actions
 
@@ -41,30 +36,21 @@ class Command:
         nouns = []
         for tok in prep.children:
             if tok.pos_ == 'NOUN':
-                print(tok.text)
                 nouns.append(tok)
         return nouns
         
     #helper function for parse() used to pick up potential undetected noun chunks
     def check_adj(self, word):
-        rightTokens = []   
-        leftTokens = []     
-        for tok in word.rights:
+        adj = []     
+        for tok in word.children:
             if tok.pos_ == 'ADP' and tok.dep_ == 'prep':
                 for n in self.check_prep(tok):
-                    rightTokens += self.check_adj(n)
+                    adj += self.check_adj(n)
             elif tok.pos_ == "NUM" or tok.pos_ == "ADJ" or tok.pos_ == "ADV" or tok.dep_ == "compound":
-                rightTokens.append(tok.lemma_)
+                    adj.append((tok.lemma_, tok.pos_))
 
-        for tok in word.lefts:
-            if tok.pos_ == 'ADP' and tok.dep_ == 'prep':
-                for n in self.check_prep(tok):
-                    leftTokens += self.check_adj(n)
-            elif tok.pos_ == "NUM" or tok.pos_ == "ADJ" or tok.pos_ == "ADV" or tok.dep_ == "compound":
-                    leftTokens.append(tok.lemma_)
-                    
-        print("check adj ->", word.text, '->', leftTokens + [word.text] + rightTokens)
-        return leftTokens + [word.text] + rightTokens
+        print("check adj ->", word.text, '->', adj + [word.text])
+        return adj + [(word.text, word.pos_)]
     
     #helper function for parse() used to get conjunctive sentence/ compound words
     #ie Find a sheep, horse, and cow -> [sheep, horse, cow]
@@ -72,16 +58,14 @@ class Command:
         objList = []
         for child in dobj.children:
             print("parse conjunction ->", dobj.text, '-> ', child.text)
-            #three cases: noun+conj, adv+appos, adp+prep
-            if child.pos_ == 'NOUN' or child.pos_ == 'ADV':
-                objList.append( {verb: self.check_adj(child)} )
-                objList += self.parse_conj(child, verb) 
+            if (child.pos_ == 'NOUN' or child.pos_ == 'ADV') and (child.dep_ == 'conj'):
+                objList.append( {verb: self.check_adj(child) }) #find adj for found noun/adv
+                objList += self.parse_conj(child, verb)  #check if there is connecting to found noun/adv
         return objList
     
-    #check prep function??
-
-    #Parses doc object and returns a list of dicts. Each dict's key is the verb and the value a list of words that has
-    #an important dependence on the verb
+    #Parses doc object and returns a list of dicts. Each dict's key is the verb and the value a list of 2 tuples
+    #each 2 tuple has an important dependence on the verb
+    #first index is the word itself the second index is the word's POS
     #kind of buggy works on grammarly correct sentences, and mixed results on more relaxed sentences
     def parse(self):
         parseList = []
@@ -89,11 +73,16 @@ class Command:
             if token.pos_ == 'VERB':
                 print("VERB -> ", token.lemma_) 
                 print("VERB CHILDREN -> ", [c.text for c in token.children])
+                neg = 0
                 dobjs = []
                 pair = {token.lemma_: dobjs}
                 for child in token.children:
                     print("VERB CHILD ->", child.text)
-                    if child.pos_ == 'NOUN' or child.dep_ == 'dobj': #verb then noun
+                    if child.pos_ == 'ADV' and child.dep_== 'neg':
+                        neg = 1
+                        print("NEGATIVE CASE") #ie input is "do not walk left" then ignore the command
+                        break
+                    elif child.pos_ == 'NOUN' or child.dep_ == 'dobj': #verb then noun
                         print("NOUN CASE")
                         dobjs += self.check_adj(child)
                         objList = self.parse_conj(child, token.lemma_) #check for conjunctions
@@ -114,12 +103,13 @@ class Command:
                         objList = self.parse_conj(child, token.lemma_) #check for conjunctions
                         if objList:
                             parseList += objList
-                parseList.append(pair)
+                if not neg:
+                    parseList.append(pair)
 
-        print("PARSELIST BEFORE FILTER ->", parseList)
-        self.filter(parseList) #filtering
+        print("PARSELIST ->", parseList)
+        #self.filter(parseList) #filtering currently not working cause of refactoring
         print("PARSELIST AFTER FILTER ->", parseList)
-        #parseList = self.similarity(parseList) #similarity check against Command.actions
+        #parseList = self.similarity_actions(parseList) #similarity check against Command.actions
         print("PARSELIST AFTER SIMILARITY ->", parseList)
         return parseList
 
@@ -152,7 +142,7 @@ class Command:
     #jump run walk
     #check similiarty of action
     #only actions
-    def similarity(self, parseList):
+    def similarity_actions(self, parseList):
         newParseList = []
         for i,pair in enumerate(parseList):
             for k in pair.keys():
@@ -161,13 +151,6 @@ class Command:
                 newParseList.append({newKey:objList})
         return newParseList
 
-    #takes a string, returns any numerical modifier for an object as an int
-    def parse_numerical(self, s):
-        doc = nlp(s)
-        n = 1
-        for tok in doc:
-            print(tok.text, tok.pos_)
-            if tok.pos_ == "NUM":
-                n = int(tok.text)
-                break
-        return n
+    #with verb check similarity against obj1 and obj2
+    def similarity_objects(self, verb, obj1, obj2):
+        pass
--- basic_movement.py
+++ basic_movement.py
@@ -5,19 +5,28 @@ try:
 except:
     import MalmoPython
 
+import math
 import sys
 import time
 import json
 import numpy as np
-from numpy.random import randint
+from random import randint
+
+spawnBlocks = "<DrawBlock x='8' y='2' z='3' type='log' variant='oak'/>" + \
+            "<DrawBlock x='8' y='3' z='3' type='log' variant='oak'/>" + \
+            "<DrawBlock x='8' y='4' z='3' type='log' variant='oak'/>" + \
+            "<DrawBlock x='-7' y='2' z='14' type='lapis_ore'/>" + \
+            "<DrawBlock x='12' y='2' z='-5' type='iron_ore'/>" + \
+            "<DrawBlock x='-4' y='2' z='-9' type='coal_ore'/>"
 
 class BasicMovement():
 
     def __init__(self, env_config):  
         # Static Parameters
-        self.size = 50
-
-        # Malmo Parameters
+        self.size = 25
+        self.mobCount = 5   #amount of mobs per mob type
+        self.block = 0.001
+        # Malmo Parametersa
         self.agent_host = MalmoPython.AgentHost()
         world_state = self.init_malmo()
         try:
@@ -27,16 +36,25 @@ class BasicMovement():
             print(self.agent_host.getUsage())
             exit(1)
 
+    def spawn_mobs(self):
+        spawnMobs = ''
+        mobs = ['Llama', 'Cow', 'Sheep', 'Chicken', 'PolarBear', 'Pig']
+        for mob in mobs:
+            for i in range(self.mobCount):
+                spawnMobs += "<DrawEntity x='{}' y='2' z='{}' type='{}'/>".format(randint(-self.size, self.size),randint(-self.size, self.size),mob)
+        return spawnMobs
+
     def get_mission_xml(self):
+        spawnMobs = self.spawn_mobs()
         return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                 <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
                     <About>
-                        <Summary>Text-To-Steve</Summary>
+                        <Summary>Speech-To-Steve</Summary>
                     </About>
                     <ServerSection>
                         <ServerInitialConditions>
                             <Time>
-                                <StartTime>12000</StartTime>
+                                <StartTime>6000</StartTime>
                                 <AllowPassageOfTime>false</AllowPassageOfTime>
                             </Time>
                             <Weather>clear</Weather>
@@ -45,29 +63,38 @@ class BasicMovement():
                             <FlatWorldGenerator generatorString="3;7,2;1;"/>
                             <DrawingDecorator>''' + \
                                 "<DrawCuboid x1='{}' x2='{}' y1='2' y2='2' z1='{}' z2='{}' type='air'/>".format(-self.size, self.size, -self.size, self.size) + \
-                                "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='stone'/>".format(-self.size, self.size, -self.size, self.size) + \
+                                "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='grass'/>".format(-self.size, self.size, -self.size, self.size) + \
+                                spawnMobs + \
                                 '''<DrawBlock x='0'  y='2' z='0' type='air' />
-                                <DrawBlock x='0'  y='1' z='0' type='melon_block' />
-                            </DrawingDecorator>
+                                <DrawBlock x='0'  y='1' z='0' type='glowstone' />''' + \
+                                spawnBlocks + \
+                            '''</DrawingDecorator>
                             <ServerQuitWhenAnyAgentFinishes/>
                         </ServerHandlers>
                     </ServerSection>
                     <AgentSection mode="Survival">
-                        <Name>Text-To-Steve</Name>
+                        <Name>SpeechToSteve</Name>
                         <AgentStart>
-                            <Placement x="0.5" y="2" z="0.5" yaw="0"/>
+                            <Placement x="0.5" y="2" z="0.5" pitch="45" yaw="0"/>
                             <Inventory>
-                                <InventoryItem slot="0" type="diamond_pickaxe"/>
+                                <InventoryItem slot="0" type="diamond_sword"/>
+                                <InventoryItem slot="1" type="diamond_pickaxe"/>
+                                <InventoryItem slot="2" type="diamond_axe"/>
+                                <InventoryItem slot="3" type="diamond_shovel"/>
                             </Inventory>
                         </AgentStart>
                         <AgentHandlers>
                             <ContinuousMovementCommands/>
+                            <ObservationFromHotBar/>
+                            <ObservationFromNearbyEntities>''' +\
+                                "<Range name='Entities' xrange='{}' yrange='{}' zrange='{}'/>".format(self.size/2, self.size/2, self.size/2) + \
+                            '''</ObservationFromNearbyEntities>
                             <ObservationFromFullStats/>
                             <ObservationFromRay/>
                             <ObservationFromGrid>
                                 <Grid name="floorAll">
-                                    <min x="-'''+str(int(5/2))+'''" y="-1" z="-'''+str(int(5/2))+'''"/>
-                                    <max x="'''+str(int(5/2))+'''" y="0" z="'''+str(int(5/2))+'''"/>
+                                    <min x="-'''+str(int(self.size/2))+'''" y="-1" z="-'''+str(int(self.size/2))+'''"/>
+                                    <max x="'''+str(int(self.size/2))+'''" y="0" z="'''+str(int(self.size/2))+'''"/>
                                 </Grid>
                             </ObservationFromGrid>
                             <AgentQuitFromTouchingBlockType>
@@ -92,7 +119,7 @@ class BasicMovement():
 
         for retry in range(max_retries):
             try:
-                self.agent_host.startMission( my_mission, my_clients, my_mission_record, 0, 'DiamondCollector' )
+                self.agent_host.startMission( my_mission, my_clients, my_mission_record, 0, 'SpeechToSteve' )
                 break
             except RuntimeError as e:
                 if retry == max_retries - 1:
@@ -110,54 +137,78 @@ class BasicMovement():
 
         return world_state
 
+    #obs for observation
+    #can be hotbar ie Hotbar_0_item, Hotbar_1_item, ... etc
+    #Entites - return nearby mobs
+    #floorall - grid
+    #LineofSight
+    #XPos, YPos, ZPos, Pitch, Yaw
+    def get_worldstate(self, obs):
+        lastWorldState = self.agent_host.peekWorldState()
+        observation = json.loads(lastWorldState.observations[-1].text)
+        return observation[obs]
+
+    def find_entity(self, entity, num = 1):
+        entities = self.get_worldstate('Entities')
+        for ent in entities:
+            print(ent['name'], '->', ent)
+        #search for entity
+
+
     def walk_left(self, distance=1):
-        self.agent_host.sendCommand("strafe -0.5")
-        time.sleep(distance / 2.3)
-        self.agent_host.sendCommand("strafe 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("strafe -0.5")
+            time.sleep(0.45)
+            self.agent_host.sendCommand("strafe 0")
 
     def walk_right(self, distance=1):
-        self.agent_host.sendCommand("strafe 0.5")
-        time.sleep(distance / 2.3)
-        self.agent_host.sendCommand("strafe 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("strafe 0.5")
+            time.sleep(0.45)
+            self.agent_host.sendCommand("strafe 0")
 
     def walk_forward(self, distance=1):
-        self.agent_host.sendCommand("move 0.5")
-        time.sleep(distance / 2.3)
-        self.agent_host.sendCommand("move 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("move 0.5")
+            time.sleep(0.45)
+            self.agent_host.sendCommand("move 0")
 
     def walk_backward(self, distance=1):
-        self.agent_host.sendCommand("move -0.5")
-        time.sleep(distance / 2.3)
-        self.agent_host.sendCommand("move 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("move -0.5")
+            time.sleep(0.45)
+            self.agent_host.sendCommand("move 0")
 
     def run_left(self, distance=1):
-        self.agent_host.sendCommand("strafe -1")
-        time.sleep(distance / 4.9)
-        self.agent_host.sendCommand("strafe 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("strafe -1")
+            time.sleep(0.2)
+            self.agent_host.sendCommand("strafe 0")
 
     def run_right(self, distance=1):
-        self.agent_host.sendCommand("strafe 1")
-        time.sleep(distance / 4.9)
-        self.agent_host.sendCommand("strafe 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("strafe 1")
+            time.sleep(0.2)
+            self.agent_host.sendCommand("strafe 0")
 
     def run_forward(self, distance=1):
-        self.agent_host.sendCommand("move 1")
-        time.sleep(distance / 4.9)
-        self.agent_host.sendCommand("move 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("move 1")
+            time.sleep(0.2)
+            self.agent_host.sendCommand("move 0")
 
     def run_backward(self, distance=1):
-        self.agent_host.sendCommand("move -1")
-        time.sleep(distance / 4.9)
-        self.agent_host.sendCommand("move 0")
+        for i in range(distance):
+            self.agent_host.sendCommand("move -1")
+            time.sleep(0.2)
+            self.agent_host.sendCommand("move 0")
+    def jump(self, num_jumps=1):
+        for i in range(num_jumps):
+            self.agent_host.sendCommand("jump 1")
+            time.sleep(0.58)
+        self.agent_host.sendCommand("jump 0")
 
     def crouch(self, length = 2):
         self.agent_host.sendCommand("crouch 1")
         time.sleep(length)
         self.agent_host.sendCommand("crouch 0")
-
-    def jump(self, num_jumps=1):
-        for i in range(num_jumps):
-            self.agent_host.sendCommand("jump 1")
-            time.sleep(0.6)
-        self.agent_host.sendCommand("jump 0")
-
