b83733ad321be2cbc568412c1d057307ed202cad
geeeeeeeeew <39319688+geeeeeeeeew@users.noreply.github.com>
2021-03-09 16:50:34 -0800

    Update create_tracks.py

--- create_tracks.py
+++ create_tracks.py
@@ -1,41 +1,38 @@
-num_tracks = 1
-track_length = 10
+import math
+import MalmoPython
+import random
+import time
+import json
+import os
+import sys
 
-def createTracks(n, length, goal_blocks):
-	''' Creates n number of tracks/obstacles with minecarts for the agent to get through'''
+# num_tracks = 1
+track_length = 20 #number of blocks in the track
+
+
+def createTracks():
 	drawTracks = ""
-	for i in range(n):
-		goal = goal_blocks[i]
-		trackPos = (i*3)+1
-		if i == 0:
-			trackPos = i+1
-		safePos = trackPos+1
-		emptyPos = safePos+1
-		goal_block = "emerald_block"
-		if i == n-1:
-			goal_block = "diamond_block"
-		currTrack = '''	    
-							<DrawCuboid x1="''' + str(0-(length//2)) + '''" y1="4" z1="''' + str(trackPos) + '''" x2="''' + str((length//2)-1) + '''" y2="4" z2="''' + str(trackPos) + '''" type="redstone_block"/>
-							<DrawCuboid x1="''' + str(0-(length//2)-1) + '''" y1="5" z1="''' + str(trackPos) + '''" x2="''' + str(0-(length//2)-1) + '''" y2="5" z2="''' + str(trackPos) + '''" type="obsidian"/>
-							<DrawCuboid x1="''' + str((length//2)) + '''" y1="5" z1="''' + str(trackPos) + '''" x2="''' + str((length//2)) + '''" y2="5" z2="''' + str(trackPos) + '''" type="obsidian"/>
-							<DrawLine x1="''' + str(0-(length//2)) + '''" y1="5" z1="''' + str(trackPos) + '''" x2="''' + str((length//2)-1) + '''" y2="5" z2="''' + str(trackPos) + '''" type="golden_rail"/>
-							<DrawCuboid x1="''' + str(0-(length//2)) + '''" y1="4" z1="''' + str(safePos) + '''" x2="''' + str((length//2)-1) + '''" y2="4" z2="''' + str(safePos) + '''" type="netherrack"/>
-							<DrawCuboid x1="''' + str(0-(length//2)-1) + '''" y1="5" z1="''' + str(safePos) + '''" x2="''' + str(goal-1) + '''" y2="5" z2="''' + str(safePos) + '''" type="fire"/>
-							<DrawCuboid x1="''' + str(goal+1) + '''" y1="5" z1="''' + str(safePos) + '''" x2="''' + str(length//2) + '''" y2="5" z2="''' + str(safePos) + '''" type="fire"/>
-							<DrawCuboid x1="''' + str(goal) + '''" y1="4" z1="''' + str(safePos) + '''" x2="''' + str(goal) + '''" y2="4" z2="''' + str(safePos) + '''" type="''' + goal_block + '''"/>
-					'''
-		drawTracks += currTrack
-		if i % 2 == 0:
-			drawTracks += '''<DrawEntity x="''' + str(0-(length//2)) + '''" y="5" z="''' + str(trackPos+0.5) + '''" type="MinecartRideable"/>'''
-		else:
-			drawTracks += '''<DrawEntity x="''' + str((length//2)-1) + '''" y="5" z="''' + str(trackPos+0.5) + '''" type="MinecartRideable"/>'''
+	goal = random.randint(-5,5)
+	goal_block = "diamond_block"
+	drawTrack = '''	    
+			<DrawCuboid x1="''' + '-10' + '''" y1="4" z1="''' + '1' + '''" x2="''' + '9' + '''" y2="4" z2="''' + '1' + '''" type="redstone_block"/>
+			<DrawCuboid x1="''' + '-11' + '''" y1="5" z1="''' + '1'+ '''" x2="''' + '-11' + '''" y2="5" z2="''' + '1' + '''" type="obsidian"/>
+			<DrawCuboid x1="''' + '10' + '''" y1="5" z1="''' + '1'+ '''" x2="''' + '10' + '''" y2="5" z2="''' + '1' + '''" type="obsidian"/>
+			<DrawLine x1="''' + '-10' + '''" y1="5" z1="''' + '1' + '''" x2="''' + '9' + '''" y2="5" z2="''' + '1' + '''" type="golden_rail"/>
+			<DrawCuboid x1="''' + '-10' + '''" y1="4" z1="''' + '2' + '''" x2="''' + '9' + '''" y2="4" z2="''' + '2' + '''" type="netherrack"/>
+			<DrawCuboid x1="''' + '-11' + '''" y1="5" z1="''' + '2' + '''" x2="''' + str(goal-1) + '''" y2="5" z2="''' + '2' + '''" type="fire"/>
+			<DrawCuboid x1="''' + str(goal+1) + '''" y1="5" z1="''' + '2' + '''" x2="''' + '10' + '''" y2="5" z2="''' + '2' + '''" type="fire"/>
+			<DrawCuboid x1="''' + str(goal) + '''" y1="4" z1="''' + '2' + '''" x2="''' + str(goal) + '''" y2="4" z2="''' + '2' + '''" type="''' + goal_block + '''"/>
+	'''
+	drawTrack += '''<DrawEntity x="''' + str(-10) + '''" y="5" z="''' + '1.5' + '''" type="MinecartRideable"/>'''
+	# set player position
 	return '''<DrawingDecorator>
 						''' + drawTracks + '''
 			  </DrawingDecorator>'''
 
 
 
-def GetMissionXML(n_tracks, length, goal_blocks):
+def GetMissionXML():
 	return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 			<Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 			  <About>
@@ -51,7 +48,7 @@ def GetMissionXML(n_tracks, length, goal_blocks):
 			  </ServerInitialConditions>
 			  <ServerHandlers>
 				  <FlatWorldGenerator generatorString="biome_1" forceReset="true" />
-				  ''' + createTracks(n_tracks, length, goal_blocks) + '''
+				  ''' + createTracks() + '''
 				  <ServerQuitWhenAnyAgentFinishes/>
 				</ServerHandlers>
 			  </ServerSection>
@@ -100,7 +97,7 @@ if __name__ == '__main__':
   max_retries = 3
   for retry in range(max_retries):
     try:
-      agent_host.startMission(my_mission, my_client_pool, my_mission_record, 0, "AI")
+      agent_host.startMission(my_mission, my_client_pool, my_mission_record, 0, "cart_test")
       break
     except RuntimeError as e:
       if retry == max_retries - 1:
